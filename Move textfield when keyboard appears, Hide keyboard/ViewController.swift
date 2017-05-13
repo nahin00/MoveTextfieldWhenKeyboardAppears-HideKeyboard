89dbfa983433a406062a2d8e65e2e70336b943a6
nahin00 <contact.nahin@gmail.com>
2017-05-13 13:25:08 +0600

    Initial commit

--- Move textfield when keyboard appears, Hide keyboard/ViewController.swift
+++ Move textfield when keyboard appears, Hide keyboard/ViewController.swift
@@ -10,16 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var nameLbl: UILabel!
+   
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var nameTextFieldBottomConstraint: NSLayoutConstraint!
+    
+    
+    @IBOutlet weak var postBtn: UIButton!
+    @IBOutlet weak var postBtnBottomConstraint: NSLayoutConstraint!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+//        self.hideKeyboardWhenTappedAround()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(ViewController.dismissKeyboard))
+        
+        view.addGestureRecognizer(tap)
+        
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWasShown), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(ViewController.keyboardWasShown), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+        
+    }
+    
+    func dismissKeyboard() {
+        
+        view.endEditing(true)
+        
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.nameTextFieldBottomConstraint.constant = 8
+            self.postBtnBottomConstraint.constant = 8
+        })
+    }
+    
+    
+    
+    deinit {
+        NotificationCenter.default.removeObserver(self);
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+ 
+    func keyboardWasShown(notification: NSNotification) {
+        
+        let info = notification.userInfo!
+        let keyboardFrame: CGRect = (info[UIKeyboardFrameEndUserInfoKey] as! NSValue).cgRectValue
+        
+        UIView.animate(withDuration: 0.1, animations: { () -> Void in
+            self.nameTextFieldBottomConstraint.constant = keyboardFrame.size.height + 20
+            self.postBtnBottomConstraint.constant = keyboardFrame.size.height + 20
+        })
     }
+    
+    
+    
+    @IBAction func postBtnTapped(_ sender: UIButton) {
+        
+        guard let name = nameTextField.text else {
+            return
+        }
+        
+        nameLbl.text = name
+    }
+    
+    
 
 
 }
 
+//extension UIViewController {
+//    func hideKeyboardWhenTappedAround() {
+//        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+//        tap.cancelsTouchesInView = false
+//        view.addGestureRecognizer(tap)
+//    }
+//
+//    func dismissKeyboard() {
+//        view.endEditing(true)
+//    }
+//}
