f1fd9bfb16673579c17d5c03e432a0232a3f276f
nahin00 <contact.nahin@gmail.com>
2017-05-13 12:53:27 +0600

    Initial Commit

---
+++ Move textfield when keyboard appears, Hide keyboard/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Move textfield when keyboard appears, Hide keyboard
+//
+//  Created by Nahin Ahmed on 5/13/17.
+//  Copyright © 2017 Nahin Ahmed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
